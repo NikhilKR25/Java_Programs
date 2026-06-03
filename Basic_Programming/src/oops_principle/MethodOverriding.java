b2280f5e2b3901afc866f5c1250c8f645d6b382f
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-06-03 20:33:27 +0530

    method overloading file added

---
+++ Basic_Programming/src/oops_principle/MethodOverriding.java
@@ -0,0 +1,32 @@
+package oopsPrinciple;
+
+class A{
+	public int a;
+	public int demo() {
+		System.out.println("method one class 1");
+		return 1;
+	}
+	public void meth() {
+		System.out.println("method two class 1");
+	}
+}
+class B extends A{
+	@Override
+	public void meth() {
+		System.out.println("method override in class 2");
+	}
+}
+
+
+
+public class MethodOverriding {
+
+	public static void main(String[] args) {
+		A var = new A();
+		var.demo();
+		var.meth();
+		B var1 = new B();
+		var1.meth();
+	}
+
+}
