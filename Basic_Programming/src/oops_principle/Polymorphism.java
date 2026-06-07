92abede509d7d8ffe73188b091e2fe3a849c568b
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-06-07 23:14:46 +0530

    polymorphism functional file

---
+++ Basic_Programming/src/oops_principle/Polymorphism.java
@@ -0,0 +1,53 @@
+package oops_principle;
+
+interface MyCamera{
+    void takeSnap();
+    void recordVideo();
+    }
+
+interface MyWifi{
+    void getNetworks();
+}
+
+class MyCellPhone{
+    void callNumber(int phoneNumber){
+        System.out.println("Calling from "+ phoneNumber);
+    }
+    void pickCall(){
+        System.out.println("Connecting... ");
+    }
+
+}
+
+class MySmartPhone extends MyCellPhone implements MyWifi, MyCamera
+{
+    public void takeSnap(){
+        System.out.println("Taking snap");
+    }
+    public void recordVideo(){
+        System.out.println("Video recorded");
+    }
+    public void getNetworks(){
+        System.out.println("Getting List of Networks");
+ 
+    }
+    public void sampleMethod(){
+        System.out.println("sample of method");
+    }
+}
+public class Polymorphism 
+{
+    public static void main(String[] args) {
+        MyCamera cam1 = new MySmartPhone(); // This is a smartphone but, use it as a camera
+        // cam1.sampleMethod(); --> Not allowed
+        cam1.recordVideo();
+
+        MySmartPhone s = new MySmartPhone();
+        s.sampleMethod();
+        s.recordVideo();
+        s.getNetworks();
+        s.callNumber(79794448);
+    }
+}
+
+//A a=new B();//up-casting  
