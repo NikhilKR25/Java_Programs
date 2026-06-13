4490b3c9d6b1f224b6f706d001260ad498fa7b86
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-06-13 23:40:39 +0530

    Selection sort updated

--- Basic_Programming/src/array/ArraySortLogic.java
+++ Basic_Programming/src/array/ArraySortLogic.java
@@ -1,7 +1,8 @@
 package array;
 
 import java.util.Arrays;
-//for a Given Array print the output as 012345
+//for a Given Array print the output as 012345,
+//it also known as Selection Sort.
 public class ArraySortLogic {
 	public static void main(String[] args) 
 	{
@@ -11,17 +12,22 @@ public class ArraySortLogic {
 	}
 
 	private static int[] sortArray(int[] arr) {
-		for (int i = 0; i <arr.length; i++){
+//		selects the position where the minimum element will be placed
+		for (int i = 0; i < arr.length; i++) {
+//			current element is the minimum
 			int index = i;
-			for (int j = i ; j <= arr.length-1; j++){
-			if (arr[j] < arr[index]){
-			index = j;
+			
+//			Find the index of the smallest element
+			for (int j = i; j <= arr.length - 1; j++) {
+				if (arr[j] < arr[index]) {
+					index = j;
+				}
 			}
-			}
+//			Swap current element with the minimum element
 			int temp = arr[i];
 			arr[i] = arr[index];
 			arr[index] = temp;
-			}
+		}
 		return arr;
 	}
 }
