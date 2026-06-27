40c520477afb61e9cb29e102128f0805fb0a7b72
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-06-27 23:19:19 +0530

    Separate numbers and charaters from a string file

--- Basic_Programming/src/string/SeparateNumChar.java
+++ Basic_Programming/src/string/SeparateNumChar.java
@@ -4,17 +4,20 @@ public class SeparateNumChar{
 	public static void main(String[] args) 
 	{
 		String str = "a1b2c3d4e5";
-
+		
+		// Variables to store characters and numbers
 		String chars = " ";
 		String nums = " ";
 		
+		// Convert string into character array
 	for (char ch : str.toCharArray()) {
+		// Check whether character is a digit or letter.
 	    if (Character.isDigit(ch))
 	        nums += ch;
 	    else if (Character.isLetter(ch))
 	        chars += ch;
 	}
-
+	// Display result
 	System.out.println("Characters in a String: " + chars);
 	System.out.println("Numbers in a String: " + nums);
 		
