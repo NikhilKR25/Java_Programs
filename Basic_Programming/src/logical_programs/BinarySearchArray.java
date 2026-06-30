864d7c79d49abcd35eb065eb9b53cd52e6751418
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-06-30 22:01:03 +0530

    Binary seerch finding the element

--- Basic_Programming/src/logical_programs/BinarySearchArray.java
+++ Basic_Programming/src/logical_programs/BinarySearchArray.java
@@ -4,12 +4,15 @@ public class BinarySearchArray {
 
     public static void main(String[] args) {
 
+    	// Binary Search works only on a sorted array.
         int[] arr = {5, 6, 7, 8, 9, 10, 11};
 
+        // Element to be searche
         int key = 9;
 
         int index = binarySearch(arr, key);
 
+        // Check whether the element is found or Not.
         if (index != -1) {
             System.out.println("Element at index: " + index);
         } else {
@@ -18,14 +21,15 @@ public class BinarySearchArray {
     }
 
     public static int binarySearch(int[] arr, int key) {
-
+    	// Starting index
         int low = 0;
+        // Last index
         int high = arr.length - 1;
-//        Find the middle element.
+        //  Find the middle element.
         while (low <= high) {
 
             int mid = low + (high - low) / 2;
-//        check key value is grater or less , Repeat until the element is found or the search space becomes empty.
+            //  check key value is grater or less , Repeat until the element is found or the search space becomes empty.
             if (arr[mid] == key) {
                 return mid;
             } else if (arr[mid] < key) {
@@ -34,7 +38,7 @@ public class BinarySearchArray {
                 high = mid - 1;
             }
         }
-
+        // if element is not present in array it return
         return -1;
     }
 }
