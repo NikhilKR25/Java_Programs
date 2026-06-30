84de502837113e2b5e0bc0a34ed903f38a75ecf3
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-06-30 21:49:07 +0530

    second largest number from an Array file

--- Basic_Programming/src/array/SecondLargeNumber.java
+++ Basic_Programming/src/array/SecondLargeNumber.java
@@ -5,16 +5,20 @@ public class SecondLargeNumber {
 	public static void main(String[] args) {
 
 		int arr[] = { 10, 40, 20, 60, 80 };
-
+		
+		// Initialize largest and second largest integer values
 		int largest = Integer.MIN_VALUE;
 		int second = Integer.MIN_VALUE;
-
+		
+		// Traverse each element
 		for (int num : arr) {
 
 			if (num > largest) {
+				// The previous largest becomes the second largest
 				second = largest;
 				largest = num;
 			} else if (num > second && num != largest) {
+				// Update the largest element
 				second = num;
 			}
 		}
