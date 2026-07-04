136b7f8701fa64986cf1fc6a9f31ca126452764d
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-07-04 23:09:52 +0530

    find Fibonacci Sequence in java

--- Basic_Programming/src/logical_programs/FibonacciSequence.java
+++ Basic_Programming/src/logical_programs/FibonacciSequence.java
@@ -2,6 +2,7 @@ package logical_programs;
 
 public class FibonacciSequence {
 
+	 // Prints Fibonacci sequence up to the given count
 	public static void printFibonacciSequence(int count) {
 		int a = 0;
 		int b = 1;
@@ -9,7 +10,7 @@ public class FibonacciSequence {
 
 		for (int i = 1; i <= count; i++) {
 			System.out.print(a + ", ");
-
+			// Generate the next Fibonacci number
 			a = b;
 			b = c;
 			c = a + b;
