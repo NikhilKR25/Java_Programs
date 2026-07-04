069f70ab24143b69cf91ca74c7d29515ae134bed
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-07-04 22:48:27 +0530

    find Strong Number in Java from input

--- Basic_Programming/src/logical_programs/StrongNumber.java
+++ Basic_Programming/src/logical_programs/StrongNumber.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class StrongNumber {
 
  public static void main(String[] args) {
+	// Create Scanner object to take input from the user
      Scanner sc = new Scanner(System.in);
 
      System.out.println("Enter Number :");
@@ -14,17 +15,22 @@ public class StrongNumber {
      int temp = num;
      int r, fact, sum = 0;
 
+     // Process each digit
      while (temp > 0) {
+    	 // Extract the last digit
          r = temp % 10;
 
+         // Find factorial of the digit & Reset factorial to 1 for every new digit
          fact = 1;
          for (int i = r; i >= 1; i--) {
              fact *= i;
          }
          sum += fact;
+      // Remove the last digit from the number
          temp /= 10;
      }
 
+     // Check if it is a Strong Number
      if (num == sum) {
          System.out.println(num + " Strong Number.");
      } else {
