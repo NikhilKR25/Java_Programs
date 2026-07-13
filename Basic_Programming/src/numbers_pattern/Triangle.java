cb3ad3b101d7c6c7c2be0e4beebc8f9409031778
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-07-13 22:43:12 +0530

    Number Triangle Floyd's Triangle pattern

--- Basic_Programming/src/numbers_pattern/Triangle.java
+++ Basic_Programming/src/numbers_pattern/Triangle.java
@@ -1,16 +1,18 @@
 package numbers_pattern;
-
+//	Floyd’s Triangle 
 public class Triangle {
+	
 	public static void main(String[] args) {
 		int rows = 4;
 		int number = 1;
-
+		//	outer loop
 		for (int i = 1; i <= rows; i++) 
 		{
+			//	inner loop
 			for (int j = 1; j <= i; j++) 
 			{
 				System.out.print(number + " ");
-				number++;
+				number++; // Increment value after inner loop execute
 			}
 			System.out.println();
 		}
