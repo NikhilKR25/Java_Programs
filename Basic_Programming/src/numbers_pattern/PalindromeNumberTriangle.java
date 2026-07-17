1fbefac9a5e13b46d75cfdaf04b16ad292288cbb
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-07-17 23:16:08 +0530

    Palindrome Number Triangle pattern program

--- Basic_Programming/src/numbers_pattern/PalindromeNumberTriangle.java
+++ Basic_Programming/src/numbers_pattern/PalindromeNumberTriangle.java
@@ -3,15 +3,16 @@ package numbers_pattern;
 public class PalindromeNumberTriangle {
 	public static void main(String[] args) {
         int rows = 5;
-
+        // Outer loop controls the number of rows
         for (int i = 1; i <= rows; i++) {
-
+        	 // Print leading spaces
             for (int s = 1; s <= rows - i; s++)
                 System.out.print(" ");
 
+         // Print numbers in increasing order (1 to i)
             for (int j = 1; j <= i; j++)
                 System.out.print(j);
-
+         // Print numbers in decreasing order (i-1 to 1)
             for (int j = i - 1; j >= 1; j--)
                 System.out.print(j);
 
@@ -20,3 +21,10 @@ public class PalindromeNumberTriangle {
     }
 
 }
+
+/*
+    1
+   121
+  12321
+ 1234321 
+ */
