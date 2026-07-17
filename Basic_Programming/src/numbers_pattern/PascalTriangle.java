e579f3ec05e7d5c4b40e1c7be0a1b0a9467f6025
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-07-17 23:24:58 +0530

    Pascal Triangle

---
+++ Basic_Programming/src/numbers_pattern/PascalTriangle.java
@@ -0,0 +1,26 @@
+package numbers_pattern;
+
+public class PascalTriangle {
+	
+	public static void main(String[] args) {
+		int rows = 5;
+
+		for (int i = 0; i < rows; i++) 
+		{
+			int num = 1;
+
+			for (int s = 1; s <= rows - i; s++)
+			{
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j <= i; j++) 
+			{
+				System.out.print(num + " ");
+				num = num * (i - j) / (j + 1);
+			}
+
+			System.out.println();
+		}
+	}
+}
