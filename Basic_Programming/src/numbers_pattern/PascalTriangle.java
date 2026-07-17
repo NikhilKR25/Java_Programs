9cc1a20b0695d87e00e5625e6c5664b5ddb49d03
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-07-17 23:30:31 +0530

    Pascal Triangle number pattern

--- Basic_Programming/src/numbers_pattern/PascalTriangle.java
+++ Basic_Programming/src/numbers_pattern/PascalTriangle.java
@@ -7,19 +7,22 @@ public class PascalTriangle {
 
 		for (int i = 0; i < rows; i++) 
 		{
+			// First value in every row is 1
 			int num = 1;
 
+			// Print leading spaces to align
 			for (int s = 1; s <= rows - i; s++)
 			{
 				System.out.print(" ");
 			}
-
+			// Print the numbers in the current row
 			for (int j = 0; j <= i; j++) 
 			{
 				System.out.print(num + " ");
+				 // Next Value = Current Value × (Row - Column) ÷ (Column + 1)
 				num = num * (i - j) / (j + 1);
 			}
-
+			 // Move to the next line after completing current row
 			System.out.println();
 		}
 	}
