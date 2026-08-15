3051788948082aa5570fe1dbe7ec595b5af1c66f
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-08-15 23:02:58 +0530

    Java Object Class of arraylist

--- Basic_Programming/src/objects/ObjectClass.java
+++ Basic_Programming/src/objects/ObjectClass.java
@@ -6,18 +6,25 @@ public class ObjectClass {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
+		
+		// All three references point to the same ObjectClass object.
 		ObjectClass oc = new ObjectClass();
 		ObjectClass oc1 = oc;
 		ObjectClass oc2 = oc1;
+		
+		// getClass() returns the runtime class of the object.
 		System.out.println("Object class name :"+oc2.getClass().getName());
 		
 		ArrayList al = new ArrayList();
+		
+		// Primitive values are automatically converted to wrapper objects
 		al.add(11);
 		al.add(10.5);
 		al.add(null);
 		al.add('k');
 		System.out.println("Object :"+al);
 		
+		// clone() creates a shallow copy of the ArrayList.
 		ArrayList alc = (ArrayList)al.clone();
 		System.out.println("Clone object :"+alc);
 	}
