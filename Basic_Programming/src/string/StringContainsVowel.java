6529c4347811e39fa5a16647b0b545c51e9d9e13
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-08-18 20:08:36 +0530

    Check String contains Vowels or not

--- Basic_Programming/src/string/StringContainsVowel.java
+++ Basic_Programming/src/string/StringContainsVowel.java
@@ -5,13 +5,13 @@ public class StringContainsVowel {
 	public static void main(String[] args) {
 
 		String str = "universal";
-
+		// Flag to keep track of whether a vowel is found
 		boolean flag = false;
 
 		for (int i = 0; i < str.length(); i++) {
-
+			 // Get the current character and convert it to lowercase
 			char ch = Character.toLowerCase(str.charAt(i));
-
+			 // Check the character
 			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
 
 				flag = true;
