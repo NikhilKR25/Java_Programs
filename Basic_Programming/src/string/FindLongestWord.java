a1a03bb7bacbb852767447ebd02c4623d5c7f4ec
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-08-19 23:08:57 +0530

    find longest word

--- Basic_Programming/src/string/FindLongestWord.java
+++ Basic_Programming/src/string/FindLongestWord.java
@@ -3,16 +3,17 @@ package string;
 public class FindLongestWord {
 
     public static void main(String[] args) {
-
+    	// Store the sentence in a String variable
         String sentence="Java is an object oriented programming language";
 
         String longest="";
 
+        // Split the sentence into individual words and loop through each word
         for(String word:sentence.split(" ")){
+        	// Check if the current word is longer than the previously, 
             if(word.length()>longest.length())
                 longest=word;
         }
-
         System.out.println("Longest Word in sentence is :"+longest);
     }
 }
