46d24d8f11e67fbbd467ad133fe80fe399123d3f
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-08-20 22:31:14 +0530

    Check String Palindrom

--- Basic_Programming/src/string/palindromString.java
+++ Basic_Programming/src/string/palindromString.java
@@ -6,13 +6,17 @@ public class palindromString {
 
 	public static void main(String[] args) 
 	{
+		// Create Scanner object to take input from the user
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter String : ");
 		String str = scan.next();
+		
 		System.out.println("Enter String: "+str);
 		System.out.println("----------------------");
 		
+		// Convert the string into a character array
 		char[] arr = str.toCharArray();
+		// Variable to store the reversed string
 		String rev = "";
 		for(int i=arr.length-1; i>=0; i--)
 		{
@@ -21,6 +25,7 @@ public class palindromString {
 		System.out.println("Reversed String: "+rev);			
 		System.out.println("-----------------------");
 		
+		// Compare original and reversed strings
 		if(str.equals(rev))
 		{
 			System.out.println("Palindrome String . . .");
