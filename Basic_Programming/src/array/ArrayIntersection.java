58b0c1beff67c6c6c3ab77cbf72fb4f3335814e2
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-02 23:07:22 +0530

    Array intersect - by commaring both array find Common Words

--- Basic_Programming/src/array/ArrayIntersection.java
+++ Basic_Programming/src/array/ArrayIntersection.java
@@ -5,15 +5,19 @@ import java.util.HashSet;
 public class ArrayIntersection {
 
 	public static void main(String[] args) {
+		
+		// Declaring String Array
 		String[] s1 = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"};  
         String[] s2 = {"THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT"};
-  
+        
+        // Hashset for common elements
         HashSet<String> set = new HashSet<String>();
         for (int i = 0; i < s1.length; i++)
         {
             for (int j = 0; j < s2.length; j++)
             {
-                if(s1[i].equals(s2[j]))
+                //	check if elements of both arrays
+            	if(s1[i].equals(s2[j]))
                 {
                     set.add(s1[i]);
                 }
