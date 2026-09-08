1c136aec3219fc58a7f395728d69b3b66846a55e
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-08 17:41:58 +0530

    logic for Solid Sqaure pattern

--- Basic_Programming/src/normal_pattern/SolidSquare.java
+++ Basic_Programming/src/normal_pattern/SolidSquare.java
@@ -1,5 +1,21 @@
 package normal_pattern;
 
 public class SolidSquare {
-
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Program Started..");
+		
+		int lines = 5;
+		int starCount = 5;
+		for(int i=1; i<=lines; i++)
+		{
+			for(int j=1; j<=starCount; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("Program Ended");
+	}
 }
