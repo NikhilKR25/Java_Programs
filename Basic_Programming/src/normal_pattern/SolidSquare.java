09328cbfed1af4de8f762b8b99948b7a69b73ae9
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-08 17:49:19 +0530

    Solid Sqaure Star Pattern in Java

--- Basic_Programming/src/normal_pattern/SolidSquare.java
+++ Basic_Programming/src/normal_pattern/SolidSquare.java
@@ -5,17 +5,25 @@ public class SolidSquare {
 	public static void main(String[] args) 
 	{
 		System.out.println("Program Started..");
-		
+		//	numbers of rows and stars
 		int lines = 5;
 		int starCount = 5;
+		//	outer loop for number of rows
 		for(int i=1; i<=lines; i++)
 		{
+			//	inner loop for numbers of stars
 			for(int j=1; j<=starCount; j++)
 			{
-				System.out.print("*");
+				System.out.print("*"); // print stars on the same line
 			}
 			System.out.println();
 		}
 		System.out.println("Program Ended");
 	}
 }
+/*
+*****
+*****
+*****
+*****
+*/
