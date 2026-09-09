7f66e5bcfd7548e559898181997533f1fb26ed86
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-09 17:50:34 +0530

    Hollow Rectangle pattern

---
+++ Basic_Programming/src/normal_pattern/HollowRectangle.java
@@ -0,0 +1,22 @@
+package normal_pattern;
+
+public class HollowRectangle {
+
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= columns; j++) {
+
+                if (i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
