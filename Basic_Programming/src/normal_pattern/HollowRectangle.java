7e0b426acc4694a600c4f1310045d49d53242d6b
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-10 10:52:29 +0530

    Hollow Rectangle Star Pattern in Java

--- Basic_Programming/src/normal_pattern/HollowRectangle.java
+++ Basic_Programming/src/normal_pattern/HollowRectangle.java
@@ -3,20 +3,34 @@ package normal_pattern;
 public class HollowRectangle {
 
     public static void main(String[] args) {
+    	//	numbers of rows and columns
         int rows = 4;
         int columns = 5;
 
+        // Outer loop for rows
         for (int i = 1; i <= rows; i++) {
 
+            // Inner loop for columns
             for (int j = 1; j <= columns; j++) {
 
+                // to Print * on the border
                 if (i == 1 || i == rows || j == 1 || j == columns) {
                     System.out.print("*");
                 } else {
+                    // to Print space
                     System.out.print(" ");
                 }
             }
+            // Move to the next row
             System.out.println();
         }
     }
 }
+/* output
+
+*****
+*   *
+*   *
+*****
+
+ */
