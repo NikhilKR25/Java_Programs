a83b114bb783e706c8e72037c25ced48cca9da87
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-10 22:15:18 +0530

    Half Pyramid star pattern/ right traingle in Java

--- Basic_Programming/src/normal_pattern/HalfPyramid.java
+++ Basic_Programming/src/normal_pattern/HalfPyramid.java
@@ -5,14 +5,22 @@ public class HalfPyramid {
 
         int rows = 4;
 
+        // Outer loop controls the rows
         for (int i = 1; i <= rows; i++) 
         {
+            // Inner loop prints stars
             for (int j = 1; j <= i; j++) 
             {
                 System.out.print("*");
             }
+            // Move to the next row
             System.out.println();
         }
     }
 }
-
+/*
+*
+**
+***
+**** 
+*/
