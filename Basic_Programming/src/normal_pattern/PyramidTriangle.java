8011341dff3f6d3e0147031735a405d68cf28b27
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-12 16:53:55 +0530

    Pyramid Triangle Pattern in java

--- Basic_Programming/src/normal_pattern/PyramidTriangle.java
+++ Basic_Programming/src/normal_pattern/PyramidTriangle.java
@@ -6,17 +6,26 @@ public class PyramidTriangle {
 		int rows = 5;
 		int spaceCount = rows -1;
 		int starCount = 1;
+		// Outer loop controls the rows
 		for(int i=1; i<=rows; i++)
 		{
+			 // Inner loop prints stars and spaces
 			for(int j=1; j<=spaceCount; j++) {
 				System.out.print(" ");
 			}
 			for(int k=1; k<=starCount; k++)	{
 				System.out.print("*");
 			}
-				spaceCount--;	
-				starCount+=2;	
+				spaceCount--;	// spaceCount decreases by 1
+				starCount+=2;	// starCount increases by 2
 				System.out.println();
 		}
 	}
 }
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ */
