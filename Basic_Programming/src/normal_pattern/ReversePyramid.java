e2aa0d44c59147d72c53ab51ad8d3f9ae52cde39
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-14 21:58:16 +0530

    Reverse Pyramid star pattern in Java

--- Basic_Programming/src/normal_pattern/ReversePyramid.java
+++ Basic_Programming/src/normal_pattern/ReversePyramid.java
@@ -2,12 +2,16 @@ package normal_pattern;
 
 public class ReversePyramid {
 	public static void main(String[] args) {
+		
 		int rows = 4;
 		int spaceCount = 0;
+		// For rows, maximum stars will be 7
 		int starCount = rows * 2 - 1;
-
-		for (int i = 1; i <= rows; i++) {
-
+		
+		// Outer loop controls the rows
+		for (int i = 1; i <= rows; i++) 
+		{
+			 // Inner loop prints stars and spaces
 		    for (int j = 1; j <= spaceCount; j++) {
 		        System.out.print(" ");
 		    }
@@ -16,8 +20,8 @@ public class ReversePyramid {
 		        System.out.print("*");
 		    }
 
-		    spaceCount++;
-		    starCount -= 2;
+		    spaceCount++;	// starCount increases by 1
+		    starCount -= 2;	// spaceCount decreases by 2
 
 		    System.out.println();
 		}
