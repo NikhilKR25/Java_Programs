07669cd479da65eabf1ed75a61e30c1ee28f6241
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-15 22:34:39 +0530

    Diamond pattern Class added

---
+++ Basic_Programming/src/normal_pattern/DiamondPattern.java
@@ -0,0 +1,16 @@
+package normal_pattern;
+
+public class DiamondPattern {
+	public static void main(String[] args) 
+	{
+		// Total number of rows
+	    int rows = 7;                 
+	    int starCount = 1;  
+	    // Initial spaces = 2 for 7 rows
+	    int spaceCount = rows / 2;    
+
+	    // Find the middle line
+	    int mid = (rows + 1) / 2;
+
+	}
+}
