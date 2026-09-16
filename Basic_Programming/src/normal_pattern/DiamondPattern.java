e2319103867a1745bc2728e3222de780c1ab6af9
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-16 23:25:00 +0530

    Diamond star Pattern in java

--- Basic_Programming/src/normal_pattern/DiamondPattern.java
+++ Basic_Programming/src/normal_pattern/DiamondPattern.java
@@ -12,5 +12,35 @@ public class DiamondPattern {
 	    // Find the middle line
 	    int mid = (rows + 1) / 2;
 
+	    // Loop through each row
+	    for (int i = 1; i <= rows; i++)
+	    {
+	        // Print spaces before the stars
+	        for (int j = 1; j <= spaceCount; j++)
+	        {
+	            System.out.print(" ");
+	        }
+
+	        // Print stars
+	        for (int k = 1; k <= starCount; k++)
+	        {
+	            System.out.print("*");
+	        }
+	       
+	        System.out.println();
+
+	        if (i < mid)
+	        {
+	            // Decrease spaces by 1 & Increase stars by 2
+	            spaceCount--;
+	            starCount += 2;
+	        }
+	        else
+	        {
+	            // After the middle, increase spaces by 1 & Decrease stars by 2
+	            spaceCount++;
+	            starCount -= 2;
+	        }
+	    }
 	}
 }
