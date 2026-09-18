47e0214a1521c720d24d84a20c7e70e97978fa6c
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2026-09-18 13:48:36 +0530

    Hollow Diamond Pattern class added

---
+++ Basic_Programming/src/normal_pattern/HollowDiamond.java
@@ -0,0 +1,15 @@
+package normal_pattern;
+
+public class HollowDiamond {
+	public static void main(String[] args)
+	{
+	    int rows = 7;
+
+	    int starCount = 1;
+	    int spaceCount = rows / 2;
+
+	    int mid = (rows + 1) / 2;
+
+	    
+	}
+}
